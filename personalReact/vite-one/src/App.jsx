13989b27f37fb57aabbfbf91ff9eead40866ca40
Anurag singh <anuragsinghrock143@gmail.com>
2025-08-24 22:21:50 +0530

    written some comment in the app component in the vite one

--- personalReact/vite-one/src/App.jsx
+++ personalReact/vite-one/src/App.jsx
@@ -1,7 +1,5 @@
 
 function App() {
-  
-
   return (
     <>
       <h1>This is the hello world from the react vite</h1>
@@ -10,3 +8,5 @@ function App() {
 }
 
 export default App
+
+// The major differenct between the creating the react app with create-react-app and create vite@latest is that the second one is simpler and lighter and faster one in dependencies by default only get react and react-dom in the vite one but in the CRA we get various more dependencies like 

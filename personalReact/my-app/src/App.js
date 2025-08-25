516ded8fb79720064a221f93a976389aedc5f583
Anurag singh <anuragsinghrock143@gmail.com>
2025-08-25 16:59:57 +0530

    Fix: added my-app files instead of submodule

---
+++ personalReact/my-app/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <div className="App">
+     <h1> this is the basic react app using Create react app</h1>
+     <Chai/>
+    </div>
+  );
+}
+
+export default App;
---
+++ personalReact/my-app/src/chai.js
@@ -0,0 +1,7 @@
+function Chai (){
+    return(
+        <h2>This is the react with the create react app</h2>
+    )
+}
+
+export default Chai;
